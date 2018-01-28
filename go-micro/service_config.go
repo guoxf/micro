d43a509b9f334b7c4750fcad9928031f193a26da
elvin <ants.guoxf@gmailc.om>
2018-01-28 15:54:56 +0800

    [Feature]从环境变量中获取配置

--- go-micro/service_config.go
+++ go-micro/service_config.go
@@ -2,6 +2,10 @@ package micro
 
 import (
 	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -47,6 +51,113 @@ type ServiceConfig struct {
 	RegisterInterval     string   `yaml:"registerInterval"`
 }
 
+//LoadFromEnv 如果未指定，从环境变量中获取
+func (sc *ServiceConfig) LoadFromEnv() {
+	if sc.Client == "" {
+		sc.Client = os.Getenv("MICRO_CLIENT")
+	}
+
+	if sc.ClientRequestTimeout == "" {
+		sc.ClientRequestTimeout = os.Getenv("MICRO_CLIENT_REQUEST_TIMEOUT")
+	}
+
+	if sc.ClientRetries <= 0 {
+		retries := os.Getenv("MICRO_CLIENT_RETRIES")
+		if retries != "" {
+			r, err := strconv.ParseInt(retries, 10, 32)
+			if err != nil {
+				log.Println(err)
+			} else {
+				sc.ClientRetries = int(r)
+			}
+		}
+	}
+
+	if sc.ClientPoolSize <= 0 {
+		str := os.Getenv("MICRO_CLIENT_POOL_SIZE")
+		if str != "" {
+			poolSize, err := strconv.ParseInt(str, 10, 32)
+			if err != nil {
+				log.Println(err)
+			} else {
+				sc.ClientPoolSize = int(poolSize)
+			}
+		}
+	}
+
+	if sc.ClientPoolTTL == "" {
+		sc.ClientPoolTTL = os.Getenv("MICRO_CLIENT_POOL_TTL")
+	}
+
+	if sc.Server == "" {
+		sc.Server = os.Getenv("MICRO_SERVER")
+	}
+
+	if sc.ServerAddress == "" {
+		sc.ServerAddress = os.Getenv("MICRO_SERVER_ADDRESS")
+	}
+
+	if sc.ServerAdvertise == "" {
+		sc.ServerAdvertise = os.Getenv("MICRO_SERVER_ADVERTISE")
+	}
+
+	if sc.ServerID == "" {
+		sc.ServerID = os.Getenv("MICRO_SERVER_ID")
+	}
+
+	if len(sc.ServerMetaData) == 0 {
+		metaData := os.Getenv("MICRO_SERVER_METADATA")
+		if metaData != "" {
+			sc.ServerMetaData = strings.Split(metaData, ",")
+		}
+	}
+
+	if sc.ServerName == "" {
+		sc.ServerName = os.Getenv("MICRO_SERVER_NAME")
+	}
+
+	if sc.ServerVersion == "" {
+		sc.ServerVersion = os.Getenv("MICRO_SERVER_VERSION")
+	}
+
+	if sc.Broker == "" {
+		sc.Broker = os.Getenv("MICRO_BROKER")
+	}
+
+	if len(sc.BrokerAddress) == 0 {
+		brokerAddrs := os.Getenv("MICRO_BROKER_ADDRESS")
+		if brokerAddrs != "" {
+			sc.BrokerAddress = strings.Split(brokerAddrs, ",")
+		}
+	}
+
+	if sc.Registry == "" {
+		sc.Registry = os.Getenv("MICRO_REGISTRY")
+	}
+
+	if len(sc.RegistryAddress) == 0 {
+		registryAddrs := os.Getenv("MICRO_REGISTRY_ADDRESS")
+		if registryAddrs != "" {
+			sc.RegistryAddress = strings.Split(registryAddrs, ",")
+		}
+	}
+
+	if sc.Selector == "" {
+		sc.Selector = os.Getenv("MICRO_SELECTOR")
+	}
+
+	if sc.Transport == "" {
+		sc.Transport = os.Getenv("MICRO_TRANSPORT")
+	}
+
+	if len(sc.TransportAddress) == 0 {
+		transportAddrs := os.Getenv("MICRO_TRANSPORT_ADDRESS")
+		if transportAddrs != "" {
+			sc.TransportAddress = strings.Split(transportAddrs, ",")
+		}
+	}
+}
+
 func NewServiceConfig(configPath string) *ServiceConfig {
 	if configPath == "" {
 		panic("config path is empty!")
@@ -61,5 +172,6 @@ func NewServiceConfig(configPath string) *ServiceConfig {
 	if err = yaml.Unmarshal(b, &sc); err != nil {
 		panic(err)
 	}
+	sc.LoadFromEnv()
 	return &sc
 }
